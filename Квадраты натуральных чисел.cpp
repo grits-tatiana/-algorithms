25195534bf1d458f13916547ead439b1b633c3cd
grits-tatiana <gritsenko.ty@yandex.ru>
2019-09-26 13:15:59 +0300

    First task

---
+++ Квадраты натуральных чисел.cpp
@@ -0,0 +1,24 @@
+﻿/*
+Вывести квадраты натуральных чисел от 1 до n, используя только O(n) операций сложения и
+вычитания (умножением пользоваться нельзя).
+n ≤ 1000.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int square = 0, delta = 0, n;
+    cout << "Введите n:" << endl;
+    cin >> n;
+    cout<<"Квадраты чисел от 1 до n"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        delta = i + i + 1;
+        square += delta;
+        cout << square << " ";
+    }
+    return 0;
+}
